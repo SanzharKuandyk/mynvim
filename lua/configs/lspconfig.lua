c60f511be7dbcfb67ab15904ee494dc3df6825e4
Sanzhar Kuandyk <sanzhikwantadrive@gmail.com>
2025-09-29 14:15:53 +0500

    update lspconfig

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -80,3 +80,29 @@ end
 if #enabled_servers > 0 then
     lsp.enable(enabled_servers)
 end
+
+-- Keybindings for LSP
+local buf_map = function(bufnr, mode, lhs, rhs, opts)
+    vim.api.nvim_buf_set_keymap(bufnr, mode, lhs, rhs, opts or { silent = true })
+end
+
+vim.api.nvim_create_autocmd("LspAttach", {
+    callback = function(ev)
+        local client = vim.lsp.get_client_by_id(ev.data.client_id)
+        if client and client.server_capabilities and client.server_capabilities.semanticTokensProvider then
+            client.server_capabilities.semanticTokensProvider = nil
+        end
+
+        local bufnr = ev.buf
+        buf_map(bufnr, "n", "gd", "<cmd>lua require('telescope.builtin').lsp_definitions()<CR>")
+        buf_map(bufnr, "n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>")
+        buf_map(bufnr, "n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>")
+        buf_map(bufnr, "n", "S", "<cmd>lua vim.lsp.buf.hover()<CR>")
+        buf_map(bufnr, "n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>")
+        buf_map(bufnr, "n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>")
+        buf_map(bufnr, "n", "<leader>sh", "<cmd>lua vim.lsp.buf.signature_help()<CR>")
+        buf_map(bufnr, "n", "<leader>of", "<cmd>lua vim.diagnostic.open_float()<CR>")
+        buf_map(bufnr, "n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<CR>")
+        buf_map(bufnr, "n", "]d", "<cmd>lua vim.diagnostic.goto_next()<CR>")
+    end,
+})
