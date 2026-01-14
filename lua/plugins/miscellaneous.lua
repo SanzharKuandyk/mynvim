e3a1631559313ad35c6f05803467f071a95c50c4
Sanzhar Kuandyk <sanzhikwantadrive@gmail.com>
2026-01-14 22:55:16 +0500

    for now disable incline-anki

--- lua/plugins/miscellaneous.lua
+++ lua/plugins/miscellaneous.lua
@@ -6,14 +6,14 @@ return {
             "nvim-lua/plenary.nvim",
         },
         config = function()
-            local env_utils = require("utils.env")
-            local env = env_utils.Load_env() or {}
-            require("incline-anki").setup({
-                deck = env["ANKI_DECK"] or "Default",
-                preset = "statusline",
-                rotation_interval = 30000,
-                fields_to_show = { "Sentence", "Expression" },
-            })
+            --local env_utils = require("utils.env")
+            --local env = env_utils.Load_env() or {}
+            --require("incline-anki").setup({
+            --    deck = env["ANKI_DECK"] or "Default",
+            --    preset = "statusline",
+            --    rotation_interval = 30000,
+            --    fields_to_show = { "Sentence", "Expression" },
+            --})
         end,
     },
 }
