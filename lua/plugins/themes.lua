b082fc95108e584798a99c20c94094d828989113
Sanzhar Kuandyk <sanzhikwantadrive@gmail.com>
2026-02-11 17:52:22 +0500

    add markview; add nagisa in themery; update lazy

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -1,30 +1,30 @@
 return {
-	-- Nagisa theme (provides EndOfTheWorld colorscheme)
-	{
-		"sanzharkuandyk/nagisa.nvim",
-		lazy = false,
-		priority = 1000,
-		config = function()
-			require("nagisa").setup({})
-			-- Set default colorscheme after plugin loads
-			vim.cmd.colorscheme("EndOfTheWorld")
-		end,
-	},
+    -- Nagisa theme (provides EndOfTheWorld colorscheme)
+    {
+        "sanzharkuandyk/nagisa.nvim",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            require("nagisa").setup({})
+            -- Set default colorscheme after plugin loads
+            vim.cmd.colorscheme("EndOfTheWorld")
+        end,
+    },
 
-	-- Midnight theme
-	{
-		"dasupradyumna/midnight.nvim",
-		lazy = false,
-		priority = 1000,
-	},
+    -- Midnight theme
+    {
+        "dasupradyumna/midnight.nvim",
+        lazy = false,
+        priority = 1000,
+    },
 
-	-- Theme switcher
-	{
-		"zaldih/themery.nvim",
-		cmd = "Themery",
-		opts = {
-			themes = { "EndOfTheWorld", "midnight" },
-			livePreview = true,
-		},
-	},
+    -- Theme switcher
+    {
+        "zaldih/themery.nvim",
+        cmd = "Themery",
+        opts = {
+            themes = { "Nagisa", "EndOfTheWorld", "midnight" },
+            livePreview = true,
+        },
+    },
 }
