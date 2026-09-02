9d5221401c0ef6262d4db48c463dba8e85882efc
Sanzhar Kuandyk <sanzhikwantadrive@gmail.com>
2026-09-02 14:02:51 +0500

    increase treesitter max_filesize option

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -34,7 +34,7 @@ return {
             -- Enable treesitter highlighting per buffer (skip large files)
             vim.api.nvim_create_autocmd("FileType", {
                 callback = function(args)
-                    local max_filesize = 100 * 1024 -- 100 KB
+                    local max_filesize = 500 * 1024 -- 500 KB
                     local ok, stats = pcall(vim.uv.fs_stat, vim.api.nvim_buf_get_name(args.buf))
                     if ok and stats and stats.size > max_filesize then
                         return
